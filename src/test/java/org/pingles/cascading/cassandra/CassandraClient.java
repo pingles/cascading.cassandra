d7db16e8dbb9753d9bf5c77a6330dbec17c03c7e
Paul Ingles <paul@oobaloo.co.uk>
2011-10-24 14:38:47 +0100

    A whole bunch of tidy up and the tests now pass

--- src/test/java/org/pingles/cascading/cassandra/CassandraClient.java
+++ src/test/java/org/pingles/cascading/cassandra/CassandraClient.java
@@ -1,52 +1,67 @@
 package org.pingles.cascading.cassandra;
 
-import org.apache.cassandra.thrift.*;
+import org.apache.cassandra.thrift.Cassandra;
+import org.apache.cassandra.thrift.CfDef;
+import org.apache.cassandra.thrift.InvalidRequestException;
+import org.apache.cassandra.thrift.KsDef;
+import org.apache.cassandra.thrift.SchemaDisagreementException;
 import org.apache.thrift.TException;
-import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.cassandra.thrift.TBinaryProtocol;
+import org.apache.thrift.transport.TFramedTransport;
 import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
 import org.apache.thrift.transport.TTransportException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class CassandraClient {
-    private TSocket socket;
-    private TBinaryProtocol protocol;
+    private static final Logger LOGGER = LoggerFactory.getLogger(CassandraClient.class);
+
     private Cassandra.Client client;
+    private final TTransport transport;
 
     public CassandraClient(String rpcHost, Integer rpcPort) {
-        this.socket = new TSocket(rpcHost, rpcPort);
-        this.protocol = new TBinaryProtocol(socket);
-        this.client = new Cassandra.Client(protocol);
+        TSocket socket = new TSocket(rpcHost, rpcPort);
+        transport = new TFramedTransport(socket);
+        TBinaryProtocol protocol = new TBinaryProtocol(transport);
+        client = new Cassandra.Client(protocol);
+        LOGGER.info("CassandraClient connecting to {}:{}", rpcHost, rpcPort);
     }
 
     public String createKeyspace(String keyspaceName) throws TException, SchemaDisagreementException, InvalidRequestException {
         List<CfDef> columnFamilyDefs = new ArrayList<CfDef>();
 
-        KsDef ksDef = new KsDef(keyspaceName, "org.apache.cassandra.locator.RackUnawareStrategy", columnFamilyDefs);
+        KsDef ksDef = new KsDef(keyspaceName, "org.apache.cassandra.locator.SimpleStrategy", columnFamilyDefs);
+        ksDef.strategy_options = new HashMap<String, String>() {{
+            put("replication_factor", "1");
+        }};
 
-        this.client.send_system_add_keyspace(ksDef);
+        client.send_system_add_keyspace(ksDef);
         return this.client.recv_system_add_keyspace();
     }
 
     public List<KsDef> describeKeyspaces() throws TException, InvalidRequestException {
-        this.client.send_describe_keyspaces();
-        return this.client.recv_describe_keyspaces();
+        client.send_describe_keyspaces();
+        return client.recv_describe_keyspaces();
     }
 
-    public void open() throws TTransportException {
-        this.socket.open();
+    public void open() throws InterruptedException, TTransportException {
+        transport.open();
     }
 
     public void close() {
-        this.socket.close();
+        transport.close();
     }
 
     public boolean keyspaceExists(String keyspaceName) throws TException, InvalidRequestException {
         List<KsDef> keyspaces = describeKeyspaces();
 
         for (KsDef ksDef : keyspaces) {
-            if (ksDef.name == keyspaceName) {
+            if (ksDef.name.equals(keyspaceName)) {
                 return true;
             }
         }
--- src/test/java/org/pingles/cascading/cassandra/CassandraSchemeTest.java
+++ src/test/java/org/pingles/cascading/cassandra/CassandraSchemeTest.java
@@ -10,33 +10,60 @@ import cascading.tap.Lfs;
 import cascading.tap.Tap;
 import cascading.tuple.Fields;
 import junit.framework.TestCase;
+import org.apache.cassandra.service.EmbeddedCassandraService;
+import org.apache.cassandra.thrift.KsDef;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class CassandraSchemeTest extends CassandraTest {
+@RunWith(JUnit4.class)
+public class CassandraSchemeTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CassandraSchemeTest.class);
+
+    private static final String CASSANDRA_YAML = "./src/test/resources/cassandra.yaml";
     private String inputFile = "./src/test/data/small.txt";
     private final String keyspaceName = "TestKeyspace";
     private final String columnFamilyName = "TestColumnFamily";
+    private static EmbeddedCassandraService cassandra;
 
-    @Override
-    public void setUp() throws Exception {
-        super.setUp();
+    @BeforeClass
+    public static void startCassandra() {
+        cassandra = new EmbeddedCassandraService();
+        try {
+            cassandra.start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Before
+    public void ensureTestKeyspace() throws Exception {
         CassandraClient client = new CassandraClient(getRpcHost(), getRpcPort());
         client.open();
+        StringBuilder sb = new StringBuilder();
+        for (KsDef ks : client.describeKeyspaces()) {
+            if (sb.length() > 0) {
+                sb.append(", ");
+            }
+            sb.append(ks.name);
+        }
+        LOGGER.info("Current keyspaces: {}", sb.toString());
         if (!client.keyspaceExists(keyspaceName)) {
+            LOGGER.info("Creating test keyspace {}", keyspaceName);
             client.createKeyspace(keyspaceName);
         }
         client.close();
-    }
-
-    @Override
-    public void tearDown() throws Exception {
-        super.tearDown();
     }
 
     transient private static Map<Object, Object> properties = new HashMap<Object, Object>();
@@ -55,5 +82,23 @@ public class CassandraSchemeTest extends CassandraTest {
         parseFlow.complete();
 
         assert(true);
+    }
+
+    protected Integer getRpcPort() {
+        return (Integer) getCassandraConfiguration("rpc_port");
+    }
+
+    protected String getRpcHost() {
+        return "127.0.0.1";
+    }
+
+    private Object getCassandraConfiguration(String fieldName) {
+        Yaml y = new Yaml();
+        try {
+            Map m = (Map) y.load(new FileReader(CASSANDRA_YAML));
+            return m.get(fieldName);
+        } catch (FileNotFoundException e) {
+            return null;
+        }
     }
 }
